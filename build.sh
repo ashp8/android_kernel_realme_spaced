a7c91088b37820df5d78f64193d48d322f010343
rk134 <spaced@rk134.cf>
2022-11-01 19:58:48 +0000

    kernel: Import custom building script
    Signed-off-by: rk134 <spaced@rk134.cf>

--- build.sh
+++ build.sh
@@ -3,19 +3,16 @@
 function compile() 
 {
 
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=neolit
-export KBUILD_BUILD_USER="sarthakroy2002"
+export KBUILD_BUILD_HOST="flow-eng-labs"
+export KBUILD_BUILD_USER="rk134"
 git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
-make O=out ARCH=arm64 cupida_defconfig
+make O=out ARCH=arm64 spaced_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -29,13 +26,12 @@ make -j$(nproc --all) O=out \
 
 function zupload()
 {
-git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
+git clone --depth=1 https://github.com/rk134/AnyKernel3.git AnyKernel -b flow
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 Test-OSS-KERNEL-cupida-NEOLIT.zip *
-#curl --upload-file Test-OSS-KERNEL-cupida-NEOLIT.zip https://transfer.sh/
+zip -r9 flowKernel-OSS-spaced.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer wet Test-OSS-KERNEL-cupida-NEOLIT.zip
+./transfer wet flowKernel-OSS-spaced.zip
 }
 
 compile
