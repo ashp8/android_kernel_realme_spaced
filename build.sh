d8c46ff9a9c4d14bd786a9950928eca894578263
rk134 <spaced@rk134.cf>
2022-11-07 20:02:23 +0000

    build-kernel: Update script
    Signed-off-by: rk134 <spaced@rk134.cf>

--- build.sh
+++ build.sh
@@ -6,26 +6,29 @@ function compile()
 export ARCH=arm64
 export KBUILD_BUILD_HOST="flow-eng-labs"
 export KBUILD_BUILD_USER="rk134"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+export USE_CCACHE=1
+ccache -M 5G
+git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 $HOME/toolchains/clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 $HOME/toolchains/los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $HOME/toolchains/los-4.9-32
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 spaced_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="$HOME/toolchains//clang/bin:${PATH}:$HOME/toolchains//los-4.9-32/bin:${PATH}:$HOME/toolchains//los-4.9-64/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CC="clang" \
+                      CC="ccache clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CROSS_COMPILE="$HOME/toolchains/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="$HOME/toolchains/los-4.9-32/bin/arm-linux-androideabi-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
 function zupload()
 {
+rm -rf AnyKernel
 git clone --depth=1 https://github.com/rk134/AnyKernel3.git AnyKernel -b flow
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
